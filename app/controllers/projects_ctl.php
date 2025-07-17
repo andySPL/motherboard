58a8c416e4bc9436a3ab5365d4d1a7aaa07dd093
Andy Wood <andy@andywood.net>
2025-07-17 10:49:33 +0100

    mvc class files

---
+++ app/controllers/projects_ctl.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Projects extends Authenticatable
+{
+
+    
+}
